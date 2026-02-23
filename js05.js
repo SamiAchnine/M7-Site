bbe037a35fa4e63a5aa74237575c82ed733e4950
SamiAchnine <satejeda99@gmail.com>
2026-02-23 13:25:51 -0500

    completed up to page 171

--- js05.js
+++ js05.js
@@ -10,6 +10,45 @@
       Filename: js05.js
 */
 
+window.addEventListener("load", createLightbox);
+
+function createLightbox() {
+   // lightbox container
+   let lightBox = document.getElementById("lightbox");
+
+   // parts of the lightbox
+   let lbTitle = document.createElement("h1");
+   let lbCounter = document.createElement("div");
+   let lbPrev = document.createElement("div");
+   let lbNext = document.createElement("div");
+   let lbPlay = document.createElement("div");
+   let lbImages = document.createElement("div");
+
+   // design the lightbox title
+   lightBox.appendChild(lbTitle);
+   lbTitle.id = "lbTitle";
+
+   // design the lightbox slide counter
+   lightBox.appendChild(lbCounter);
+   lbCounter.id = "lbCounter";
+
+   // design the previous slide button
+   lightBox.appendChild(lbPrev);
+   lbPrev.id = "lbPrev";
+
+   // design the next slide button
+   lightBox.appendChild(lbNext);
+   lbNext.id = "lbNext";
+
+   // design the play button
+   lightBox.appendChild(lbPlay);
+   lbPlay.id = "lbPlay";
+
+   // design the images container
+   lightBox.appendChild(lbImages);
+   lbImages.id = "lbImages";
+}
+
 window.addEventListener("load", setupGallery);
 
 function setupGallery() {
